8e95ce9ff07f8be13fc083ab404a3e657a3489bc
Arya Enrico <aryaenrico789@gmail.com>
2023-06-04 21:52:56 +0700

    add error page and change react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Clasificattion } from './component/index.jsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { ErrorPage } from './component/errorPage/index.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    errorElement:<ErrorPage/>,
+  },
+  {
+    path:"/pengujian",
+    element:<Clasificattion/>
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={ <App />}>
-      </Route>
-      <Route path='/pengujian' element={ <Clasificattion />}>
-      </Route>
-    </Routes>
-    </BrowserRouter>
-   
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
